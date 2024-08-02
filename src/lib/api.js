ec06509428b66172d8fb00c1f7cfedb10495d347
Shiv Singh <shiv@srapsware.com>
2024-08-02 16:40:00 +0530

    chore: Update API endpoint and import axios for API requests

---
+++ src/lib/api.js
@@ -0,0 +1,5 @@
+export const APIEndPoint = () => {
+  // Set the API endpoint
+
+  return "https://statistics.domaindumper.com/api/v1";
+};
--- src/src/components/DomainStatistics.js
+++ src/src/components/DomainStatistics.js
@@ -1,1 +1,23 @@
-import axios from 'axios';
+import axios from "axios";
+import React from "react";
+import APIEndPoint from "./lib/api";
+
+export const DomainStatistics = (yyyy, mm, dd) => {
+  // Make API full url
+
+  APIEndPoint = APIEndPoint + "/stats/2024/07/02/stats.json";
+  axios
+    .get("/user?ID=12345")
+    .then(function (response) {
+      // handle success
+      console.log(response);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .finally(function () {
+      // always executed
+    });
+  return "Test";
+};
