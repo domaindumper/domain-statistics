3bea5131d3ea7164166a86a5ee66668416115fdb
Shiv Singh <shiv@srapsware.com>
2024-08-02 17:21:42 +0530

    chore: Update API endpoint and import axios for API requests

--- src/src/components/DomainStatistics.js
+++ src/src/components/DomainStatistics.js
@@ -5,7 +5,8 @@ import APIEndPoint from "./lib/api";
 export const DomainStatistics = (yyyy, mm, dd) => {
   // Make API full url
 
-  APIEndPoint = APIEndPoint + "/stats/2024/07/02/stats.json";
+  APIEndPoint =
+    APIEndPoint + "/stats/" + yyyy + "/" + mm + "/" + dd + "/stats.json";
   axios
     .get(APIEndPoint)
     .then(function (response) {
