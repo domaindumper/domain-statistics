b9972c54672d9bb4d5bed1392db2645a6a8ec0b2
Shiv Singh <shiv@srapsware.com>
2024-09-09 10:34:34 +0530

    Remove site name from main navigation

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -15,7 +15,6 @@ export function MainNav({ items }: MainNavProps) {
     <div className="flex gap-6 md:gap-10">
       <Link href="/" className="flex items-center space-x-2">
         <Icons.logo className="h-6 w-6" />
-        <span className="inline-block font-bold">{siteConfig.name}</span>
       </Link>
       {items?.length ? (
         <nav className="flex gap-6">
