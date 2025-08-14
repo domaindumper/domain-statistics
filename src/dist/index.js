1280b3f4eec65d8cc52d77b50d5965973e174a8f
Shiv Singh <shiv@srapsware.com>
2025-08-14 11:23:19 +0530

    Remove dist and src directories, migrate to nodejs
    Deleted all files from dist and src directories and moved their contents to a new src/nodejs directory. This refactor consolidates the codebase under src/nodejs for improved organization and maintainability.

--- src/dist/index.js
+++
@@ -1,6 +0,0 @@
-import DomainStatistics from './components/DomainStatistics';
-import TldList from './components/TldList';
-import RegistrarsDetails from './components/RegistrarsDetails';
-import RootZoneDatabase from './components/RootZoneDatabase';
-import TldStatistics from './components/TldStatistics';
-export { DomainStatistics, TldList, RegistrarsDetails, RootZoneDatabase, TldStatistics };
